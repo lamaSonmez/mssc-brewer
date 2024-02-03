7c21893a9a1aa5cd25cb25e58f178e20650b171a
LAMASONMEZ <lamasonmez@gmail.com>
2024-02-03 22:18:22 +0300

    initial commit

---
+++ src/main/java/gurur/springframework/msscbrewer/MsscBrewerApplication.java
@@ -0,0 +1,13 @@
+package gurur.springframework.msscbrewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBrewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gurur/springframework/msscbrewer/MsscBrewerApplicationTests.java
@@ -0,0 +1,13 @@
+package gurur.springframework.msscbrewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
